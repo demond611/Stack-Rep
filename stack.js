70ec5b6c9548a6f3b8d15a7ddd60741af6adaa4f
Robert Walker <demond611@gmail.com>
2015-09-06 14:11:43 -0500

    updated stack_rep commit

--- stack.js
+++ stack.js
@@ -1,4 +1,5 @@
 $( function() {
+
 	$('.unanswered-getter').submit( function(event){
 		// zero out results if previous search has run
 		$('.results').html('');
@@ -6,6 +7,15 @@ $( function() {
 		var tags = $(this).find("input[name='tags']").val();
 		getUnanswered(tags);
 	});
+
+	$('.inspiration-getter').submit( function(event){
+		// zero out results if previous search has run
+		$('.results').html('');
+		// get the value of the tags the user submitted
+		var answerers = $(this).find("input[name='answerers']").val();
+		topAnswers(answerers);
+	});
+
 });
 
 // this function takes the question object returned by StackOverflow 
@@ -78,6 +88,78 @@ var getUnanswered = function(tags) {
 		$.each(result.items, function(i, item) {
 			var question = showQuestion(item);
 			$('.results').append(question);
+		});
+	})
+	.fail(function(jqXHR, error, errorThrown){
+		var errorElem = showError(error);
+		$('.search-results').append(errorElem);
+	});
+};
+
+
+/////////////////////////   TOP ANSWERS SECTION   /////////////////////////
+
+
+var showAnswerer = function(answer) {
+	// clone the answers
+	var result = $(".templates .answerers").clone();
+	// set the user name to display in the result
+	var userName = result.find('.answer-name a');
+	userName.attr("href", answer.user.link);
+	userName.text(answer.user.display_name);
+
+	// set the user reputation to display in the result
+	var userReputation = result.find(".answer-reputation");
+	userReputation.text(answer.user.reputation);
+
+	// set the user score to display in the result
+	var userScore = result.find(".answer-score");
+	userScore.text(answer.score);
+
+	// set the user type to display in the result
+	var user = result.find(".user");
+	user.text(answer.user.user_type);
+
+	return result;
+};
+
+
+// this function takes the results object from StackOverflow
+// and creates info about search results to be appended to DOM
+var showSearchResults = function(query, resultNum) {
+	var results = resultNum + ' results for <strong>' + query;
+	return results;
+};
+
+// takes error string and turns it into displayable DOM element
+var showError = function(error){
+	var errorElem = $('.templates .error').clone();
+	var errorText = '<p>' + error + '</p>';
+	errorElem.append(errorText);
+};
+
+// takes a string of semi-colon separated tags to be searched
+// for on StackOverflow
+var topAnswers = function(answerers) {
+	
+	// the parameters we need to pass in our request to StackOverflow's API
+	var request = {	tag: answerers,
+					site: 'stackoverflow'};
+	
+	var result = $.ajax({
+		url: "http://api.stackexchange.com/2.2/tags/"+request.tag+"/top-answerers/all_time",
+		data: request,
+		dataType: "jsonp",
+		type: "GET",
+		})
+	.done(function(result){
+		var searchResults = showSearchResults(request.tag, result.items.length);
+
+		$('.search-results').html(searchResults);
+
+		$.each(result.items, function(i, item) {
+			var answers = showAnswerer(item);
+			$('.results').append(answers);
 		});
 	})
 	.fail(function(jqXHR, error, errorThrown){
